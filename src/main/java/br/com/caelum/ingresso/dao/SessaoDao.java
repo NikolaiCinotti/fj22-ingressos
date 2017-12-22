8028b1a843a57848e57ffaa1977bbde5a98011ad
Nikolai <agencianiko@gmail.com>
2017-12-22 19:20:27 -0200

    Adicionando tela e logica para escolha de lugares

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -13,6 +13,7 @@ import br.com.caelum.ingresso.model.Sessao;
 
 @Repository
 public class SessaoDao {
+
 	@PersistenceContext
 	private EntityManager manager;
 
@@ -28,5 +29,9 @@ public class SessaoDao {
 	public List<Sessao> buscaSessoesDoFilme(Filme filme) {
 		return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
 				.setParameter("filme", filme).getResultList();
+	}
+
+	public Sessao findOne(Integer id) {
+		return manager.find(Sessao.class, id);
 	}
 }
