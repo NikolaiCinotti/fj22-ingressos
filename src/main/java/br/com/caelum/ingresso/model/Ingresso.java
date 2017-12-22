3f2dd700f0f27920ac816c92e3b1540cc3525480
Nikolai <agencianiko@gmail.com>
2017-12-22 21:16:16 -0200

    Selecionando Ingressos e disponibilizando lugares para usuario selecionar

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -2,11 +2,30 @@ package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
 
-import br.com.caelum.ingresso.model.descontos.Desconto;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Ingresso {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@ManyToOne
 	private Sessao sessao;
+	
 	private BigDecimal preco;
+	
+	@ManyToOne
+	private Lugar lugar;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoDeIngresso tipoDeIngresso;
 
 	/**
 	 * @deprecated hibernate only
@@ -14,9 +33,10 @@ public class Ingresso {
 	public Ingresso() {
 	}
 
-	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Lugar lugar) {
 		this.sessao = sessao;
-		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+		this.preco = tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+		this.setLugar(lugar);
 	}
 
 	public Sessao getSessao() {
@@ -25,5 +45,13 @@ public class Ingresso {
 
 	public BigDecimal getPreco() {
 		return preco;
+	}
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
 	}
 }
